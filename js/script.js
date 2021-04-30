bae3342504f8d3be29e7df39680b5844efd88d51
michelafranchini <michela.franchini.91@gmail.com>
2021-04-30 16:18:32 +0200

    stampa ordine alfabetico

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Consegna:
+// 1. chiedi all'utente il cognome
+// 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+// array lista cognomi
+var listaCognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli']; 
+console.log(listaCognomi);
+
+// chiedere cognome utente
+var cognomeUtente = prompt("Scrivi il tuo cognome");
+console.log(cognomeUtente); 
+
+// inserire cognome utente nella lista
+listaCognomi.push(cognomeUtente); 
+console.log(listaCognomi);
+
+// ordinare alfabeticamente
+listaCognomi.sort(); 
+console.log(listaCognomi);
+
+// scrivere posizione umana partendo da 1
+for (var i = 1; i < listaCognomi.length; i++) {
+    
+}
